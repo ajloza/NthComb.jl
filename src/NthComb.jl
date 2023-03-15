4265bff9b26f360f43176d91683485e9907fa0ad
Andrew Loza <77357202+ajloza@users.noreply.github.com>
2023-03-14 23:23:04 -0400

    spacing

--- src/NthComb.jl
+++ src/NthComb.jl
@@ -30,7 +30,9 @@ end
 """
 `nthcomb(n,k,m)`
 
-Compute the `m`th lexicographic combination of size `k` from collection with `n` elements with assumed values `1:n`. equivalent to `collect(combinations(1:n,k))[m]`
+Compute the `m`th lexicographic combination of size `k` from collection with `n`
+elements with assumed values `1:n`. equivalent to
+`collect(combinations(1:n,k))[m]`
     
 """
 function nthcomb(n::Integer,k::Integer,m::Integer)
