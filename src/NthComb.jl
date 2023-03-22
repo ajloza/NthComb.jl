c0a8012fa0450efa5c95293b888cbf136faeebe2
Andrew Loza <77357202+ajloza@users.noreply.github.com>
2023-03-22 10:40:06 -0400

    Update docstring

--- src/NthComb.jl
+++ src/NthComb.jl
@@ -30,8 +30,7 @@ end
 """
 `nthcomb(n,k,m)`
 
-Compute the `m`th lexicographic combination of size `k` from collection with `n`
-elements with assumed values `1:n`. equivalent to
+Compute the indices for elements in the `m`th lexicographic combination of size `k` from collection with `n`. equivalent to
 `collect(combinations(1:n,k))[m]`
     
 """
